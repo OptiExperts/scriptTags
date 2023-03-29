a88cad69264bbb01668cf630fb8e123b6200eb21
Raja Abdul Moeez <abdulmoeezraja@gmail.com>
2023-03-29 16:28:47 +0500

    Update miniApp.js

--- miniApp.js
+++ miniApp.js
@@ -26,7 +26,7 @@ fetch(`${prod_type_uri}/products.json`, {
 
     // SETTING UP HEADING AND DESCRIPTION FOR THE SLIDER
     // THIS NGROK SHOULD BE CHANGED TO THE NEW SCRIPTTAG CREATED
-    await fetch("https://88d7-39-33-193-64.in.ngrok.io/send/scriptFile", {
+    await fetch("https://rich-red-hippo-vest.cyclic.app/send/scriptFile", {
         method: "POST",
     })
     .then((resp) => resp.json())
