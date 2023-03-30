48c55b5e0cbb5cdff6bc2b4c8f04e64980c83ec7
Raja Abdul Moeez <abdulmoeezraja@gmail.com>
2023-03-30 16:16:13 +0500

    Update miniApp.js

--- miniApp.js
+++ miniApp.js
@@ -115,7 +115,7 @@ fetch(`${prod_type_uri}/products.json`, {
 
             card.classList.add("loading-overlay__spinner");
             // ADDING ITEMS TO THE CART
-            let cart_product = {items: [{"id": products[ind].variants[0].id, "quantity": 1}]};
+            let cart_product = {items: [{"id": products[ind+1].variants[0].id, "quantity": 1}]};
             let cart_response = await fetch(`https://${window.location.host}/cart/add.js`, {
                 method: "POST",
                 headers: {"Content-Type": "application/json"},
