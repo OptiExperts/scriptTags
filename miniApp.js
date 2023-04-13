dba630cb09ddda9dab7db8714d5fcf1bf9bfd059
Raja Abdul Moeez <abdulmoeezraja@gmail.com>
2023-04-13 12:40:56 +0500

    Update miniApp.js

--- miniApp.js
+++ miniApp.js
@@ -11,7 +11,7 @@ let productId, title, imgLink, priceTag;
 
 // SLIDER SECTION
 sliderDiv.classList.add("main-carousel");
-sliderDiv.style.height = "210px",
+sliderDiv.style.height = "190px",
 heading.style.fontSize = "15px";
 heading.style.textAlign = "center";
 desc.style.textAlign = "center";
